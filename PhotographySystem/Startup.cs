519340eb9f6e230147b27bb76039c9b9c077a1f7
Jason Parrish <jasonparrish8@gmail.com>
2015-09-02 17:16:00 -0600

    Set up project

---
+++ PhotographySystem/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PhotographySystem.Startup))]
+namespace PhotographySystem
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
